08af62e4bf825581cad86b6bdcd0cbe183dabdd7
Sanjith2811 <x231934383@student.ncirl.ie>
2024-12-07 13:22:03 +0000

    sds

--- app.py
+++ app.py
@@ -118,4 +118,4 @@ def logout():
     return redirect(url_for('home'))
  
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8001)
